06cf1d20fc07c4a38a593f93a9563d88ac138010
jonasbn <jonasbn@gmail.com>
2015-07-15 19:58:13 +0200

    Updated acknowledgements section

--- lib/Test/Timer.pm
+++ lib/Test/Timer.pm
@@ -569,13 +569,14 @@ You can also look for information at:
 
 =over
 
+=item * Bartosz Jakubski, reporting issue #3
+
 =item * Gabor Szabo (GZABO), suggestion for specification of interval thresholds
 even though this was obsoleted by the later introduced time_between
 
 =item * Paul Leonerd Evans (PEVANS), suggestions for time_atleast and time_atmost 
 and the handling of $SIG{ALRM}. Also bug report for addressing issue with Debian 
 packaging resulting in release 0.10
-
 
 =item * brian d foy (BDFOY), for patch to L<_run_test|/_run_test>
 
